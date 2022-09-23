13cdcebefb7d4cffd455b126101ca8f0eea6c897
lolmanurfunny <77128366+lolmanurfunny@users.noreply.github.com>
2022-09-23 04:35:19 -0400

    Add files via upload

---
+++ BetaApp Patch Nabber.py
@@ -0,0 +1,34 @@
+import os
+from sys import exit
+from time import sleep
+from urllib.request import urlopen, urlretrieve
+from json import loads
+
+a = urlopen("https://clientsettingscdn.roblox.com/v2/client-version/WindowsPlayer/channel/zflag").read()
+a = loads(a)["clientVersionUpload"]
+print("Client version hash: "+a)
+filepath = os.getenv("LOCALAPPDATA")+"\Roblox\Versions\\"
+
+if os.path.exists(filepath):
+    print("Found \"\Roblox\Versions\\\" folder!")
+    filepath+=a
+    if os.path.exists(filepath):
+        print("Found \""+a+"\" folder!")
+    else:
+        input("[Error]: Unable to locate latest roblox client!")
+        exit()
+else:
+    input("[Error]: Could not find the \"\Roblox\Versions\\\" folder!")
+    exit()
+rpl = "RobloxPlayerLauncher.exe"
+
+print("Downloading latest patched launcher from github!")
+download = urlretrieve("https://github.com/lolmanurfunny/Roblox-Launcher-minus-the-app/raw/main/"+rpl,filepath+"\\"+rpl)
+
+
+print("File is located @",download.__getitem__(0)+"\nThis window will close in 3 seconds...")
+for _ in range(0,3):
+    sleep(.5)
+    print('.')
+    sleep(.5)
+exit(1)
