044e70431e346d509c4c820e152e7e54a3627a68
lolmanurfunny <77128366+lolmanurfunny@users.noreply.github.com>
2022-09-24 15:07:23 -0400

    Update Nonproprietary Roblox Launcher.py

--- Custom Launcher/Nonproprietary Roblox Launcher.py
+++ Custom Launcher/Nonproprietary Roblox Launcher.py
@@ -1,5 +1,5 @@
 #===============================================#
-#|  Nonproprietary Roblox Launcher   [v1.0.0]  |#
+#|  Nonproprietary Roblox Launcher   [v1.0.1]  |#
 #===============================================#
 from argparse import ArgumentParser
 from urllib.request import urlopen
@@ -8,28 +8,29 @@ from subprocess import Popen
 from time import sleep
 from json import loads
 from sys import exit
-import os
-import re
+from os import getenv
+from os.path import exists
+from re import split,subn
 
 
 parser = ArgumentParser()
 parser.add_argument("a")
 args = parser.parse_args().a
-found = re.split("\+",args)
+found = split("\+",args)
 
-s = [re.split(":",_)[1] for _ in found]
+s = [split(":",_)[1] for _ in found]
 args = "--InBrowser -t "+s[2]+" -j "+"\""+unquote(s[4])+"\""+" -b "+s[5]+" --launchtime="+s[3]+" --rloc "+s[6]+" --gloc "+s[7]+" -channel zflag"
-s,_ = re.subn(s[2],r"***********",args) # Censoring auth ticket, I don't like that entirely being visible on screen, word-wrap doesn't help either lol
+s,_ = subn(s[2],r"***********",args) # Censoring auth ticket, I don't like that entirely being visible on screen, word-wrap doesn't help either lol
 print(s)
 
 clientHash = loads(urlopen("https://clientsettingscdn.roblox.com/v2/client-version/WindowsPlayer/channel/zflag").read())["clientVersionUpload"]
 print("Client version hash: "+clientHash)
-filepath = os.getenv("LOCALAPPDATA")+"\Roblox\Versions\\"
+filepath = getenv("LOCALAPPDATA")+"\Roblox\Versions\\"
 
-if os.path.exists(filepath):
+if exists(filepath):
     print("Found \"\Roblox\Versions\\\" folder!")
     filepath+=clientHash
-    if os.path.exists(filepath):
+    if exists(filepath):
         print("Found \""+clientHash+"\" folder!")
     else:
         input("[Error]: Unable to locate latest roblox client!")
