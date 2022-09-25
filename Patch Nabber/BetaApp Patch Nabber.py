c53a250d4cff235c921a4008a88641b75d97aba3
lolmanurfunny <77128366+lolmanurfunny@users.noreply.github.com>
2022-09-25 04:36:48 -0400

    minor fixes
    (hehe get it)
    *proceeds to open a new issue*

--- Patch Nabber/BetaApp Patch Nabber.py
+++ Patch Nabber/BetaApp Patch Nabber.py
@@ -1,6 +1,6 @@
 from sys import exit
 from time import sleep
-from os import getenv
+from os import getenv, linesep,system
 from os.path import exists
 from urllib.request import urlopen, urlretrieve
 from json import loads
@@ -10,26 +10,34 @@ a = loads(a)["clientVersionUpload"]
 print("Client version hash: "+a)
 filepath = getenv("LOCALAPPDATA")+"\Roblox\Versions\\"
 
-if exists(filepath):
+def littleTimmyPrevention():
+    input("Did you install Roblox via running RobloxPlayerLauncher as administrator?\nTry reinstalling Roblox.\nDon't open a new issue regarding this if you installed Roblox to a non-User directory!")
+
+debug = False
+if exists(filepath) and not debug:
     print("Found \"\Roblox\Versions\\\" folder!")
     filepath+=a
     if exists(filepath):
         print("Found \""+a+"\" folder!")
     else:
-        input("[Error]: Unable to locate latest roblox client!")
-        exit()
+        print("[Error]: Unable to locate latest roblox client!")
+        littleTimmyPrevention()
+        exit(1)
 else:
-    input("[Error]: Could not find the \"\Roblox\Versions\\\" folder!")
-    exit()
+    print("[Error]: Could not find the \"\Roblox\Versions\\\" folder!")
+    littleTimmyPrevention()
+    exit(1)
+
+repo = "lolmanurfunny/Roblox-Launcher-minus-the-app"
 rpl = "RobloxPlayerLauncher.exe"
-
-print("Downloading latest patched launcher from github!")
-download = urlretrieve("https://github.com/lolmanurfunny/Roblox-Launcher-minus-the-app/raw/main/"+rpl,filepath+"\\"+rpl)
-
+latest = urlopen("https://raw.githubusercontent.com/"+repo+"/main/Custom%20Launcher/latest").read().decode().rstrip(linesep)
+#input(latest) # debuggerydoos
+print("Installing latest custom launcher from github! Version: "+latest)
+download = urlretrieve("https://github.com/"+repo+"/releases/download/"+latest+"/"+rpl,filepath+"\\"+rpl)
 
 print("File is located @",download.__getitem__(0)+"\nThis window will close in 3 seconds...")
-for _ in range(0,3):
-    sleep(.5)
+for _ in range(3,0,-1):
+    system("title "+"Closing in "+_.__str__())
     print('.')
-    sleep(.5)
-exit(1)
+    sleep(1)
+exit(0) # now exiting with a non-non-zero value :D
