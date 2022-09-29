93225552582076cf9b9fa4cfb10b487b40881276
VisualPlugin <aboshadi.n.ahmed@gmail.com>
2022-09-29 08:33:48 +0000

    Update BetaApp Patch Nabber.py

--- Patch Nabber/BetaApp Patch Nabber.py
+++ Patch Nabber/BetaApp Patch Nabber.py
@@ -5,57 +5,82 @@ from os.path import exists
 from urllib.request import urlopen, urlretrieve
 from json import loads
 
-a = urlopen("https://clientsettingscdn.roblox.com/v2/client-version/WindowsPlayer/channel/zflag").read()
-a = loads(a)["clientVersionUpload"]
-print("Client version hash: "+a)
-filepath = getenv("LOCALAPPDATA")+"\Roblox\Versions\\"
+DEBUG_MODE = False
+REPO_PATH = "lolmanurfunny/Roblox-Launcher-minus-the-app"
+ROBLOXPLAYER_NAME = "RobloxPlayerLauncher.exe"
 
-def littleTimmyPrevention():
-    input("Did you install Roblox via running RobloxPlayerLauncher as administrator?\nTry reinstalling Roblox.\nDon't open a new issue regarding this if you installed Roblox to a non-User directory!")
 
-debug = False
-if exists(filepath) and not debug:
-    print("Found \"\Roblox\Versions\\\" folder!")
-    filepath+=a
-    if exists(filepath):
-        print("Found \""+a+"\" folder!")
-    else:
-        print("[Error]: Unable to locate latest roblox client!")
-        littleTimmyPrevention()
-        exit(1)
-else:
-    print("[Error]: Could not find the \"\Roblox\Versions\\\" folder!")
-    littleTimmyPrevention()
+def uac_error():
+    input(
+        "Did you install Roblox via running RobloxPlayerLauncher as administrator?\nTry reinstalling Roblox.\n"
+        + "Don't open a new issue regarding this if you installed Roblox to a non-User directory!"
+    )
     exit(1)
 
-rpl = "RobloxPlayerLauncher.exe"
 
-location1 = filepath+"\\"+rpl
-location2 = filepath+"\content\sounds\ouch.ogg"
+def get_filepath():
+    response = urlopen(
+        "https://clientsettingscdn.roblox.com/v2/client-version/WindowsPlayer/channel/zflag"
+    ).read()
 
-repo = "lolmanurfunny/Roblox-Launcher-minus-the-app"
-latest = urlopen("https://raw.githubusercontent.com/"+repo+"/main/Custom%20Launcher/latest").read().decode().rstrip(linesep)
-#input(latest) # debuggerydoos
-print("Installing latest custom launcher from github! Version: "+latest)
-download = urlretrieve("https://github.com/"+repo+"/releases/download/"+latest+"/"+rpl,location1)
+    zflag = loads(response)["clientVersionUpload"]
+    print(f"Client version hash: {zflag}")
+    filepath = f"{getenv('LOCALAPPDATA')}\Roblox\Versions\\"
 
-print("File is located @",download.__getitem__(0))
+    if not exists(filepath) or DEBUG_MODE:
+        print('[Error]: Could not find the "\Roblox\Versions\\" folder!')
+        uac_error()
 
-oof = input("Would you like to return the oof sound back? [Y/N] ")
+    print('Found "\Roblox\Versions\\" folder!')
+    filepath += zflag
+    if exists(filepath):
+        print(f'Found "{zflag}" folder!')
+    else:
+        print("[Error]: Unable to locate latest roblox client!")
+        uac_error()
 
-if oof.lower() == "y".strip():
-    print("Installing oof sound...")
-    urlretrieve("https://github.com/"+repo+"/raw/main/Audio/ouch.ogg",location2)
-    print("Successfully installed oof sound.")
-elif oof.lower() == "n".strip():
-    print("Skipping...")
-else:
-    print("You wrote neither \"Y\" nor \"N\". Skipping...")
+    return filepath
 
-print("This window will close in 3 seconds...")
 
-for _ in range(3,0,-1):
-    system("title "+"Closing in "+_.__str__())
-    print('.')
-    sleep(1)
-exit(0) # now exiting with a non-non-zero value :D
+def reinsert_oof(filepath):
+    location = f"{filepath}\content\sounds\ouch.ogg"
+    oof = input("Would you like to return the oof sound back? [Y/N] ")
+
+    if oof.strip().lower() == "y":
+        print("Installing oof sound...")
+        urlretrieve(f"https://github.com/{REPO_PATH}/raw/main/Audio/ouch.ogg", location)
+        print("Successfully installed oof sound.")
+    elif oof.strip().lower() == "n":
+        print("Skipping...")
+    else:
+        print('You wrote neither "Y" nor "N". Skipping...')
+
+
+def install_launcher(filepath):
+    latest = (
+        urlopen(
+            f"https://raw.githubusercontent.com/{REPO_PATH}/main/Custom%20Launcher/latest"
+        )
+        .read()
+        .decode()
+        .rstrip(linesep)
+    )
+    # input(latest) # debuggerydoos
+    print(f"Installing latest custom launcher from github! Version: {latest}")
+    download = urlretrieve(
+        f"https://github.com/{REPO_PATH}/releases/download/{latest}/{ROBLOXPLAYER_NAME}",
+        f"{filepath}\\{ROBLOXPLAYER_NAME}",
+    )
+    print(f"File is located @{download.__getitem__(0)}")
+
+
+if __name__ == "__main__":
+    filepath = get_filepath()
+    install_launcher(filepath)
+    reinsert_oof(filepath)
+    print("This window will close in 3 seconds...")
+
+    for i in range(3, 0, -1):
+        system("title " + f"Closing in {i}")
+        print(".")
+        sleep(1)
