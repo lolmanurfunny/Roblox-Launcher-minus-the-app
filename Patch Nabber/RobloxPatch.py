17e4d132b63c9953411faa6a48b5a0750a7a1a79
Ziad <zizozoza1273@gmail.com>
2022-10-05 21:37:58 +0200

    a bit of optimization
    took a pull request and put it in, it was small lmao

--- Patch Nabber/RobloxPatch.py
+++ Patch Nabber/RobloxPatch.py
@@ -1,11 +1,12 @@
 from sys import exit
 from time import sleep
-import os
-from os.path import exists
+from os import getenv, linesep, system, remove
+from shutil import rmtree
+from os.path import exists, dirname, realpath
 from urllib.request import urlopen, urlretrieve
 from json import loads
 
-absPath = os.path.dirname(os.path.realpath(__file__))
+absPath = dirname(realpath(__file__))
 
 def littleTimmyPrevention():
     littleTimmyPrevention()
@@ -16,7 +17,7 @@ a = urlopen("https://clientsettingscdn.roblox.com/v2/client-version/WindowsPlaye
 a = loads(a)["clientVersionUpload"]
 print("Client version hash: "+a)
 
-filepath = os.getenv("LOCALAPPDATA")+"\Roblox\Versions\\"
+filepath = getenv("LOCALAPPDATA")+"\Roblox\Versions\\"
 
 debug = False
 if exists(filepath) and not debug:
@@ -39,25 +40,25 @@ location1 = filepath+"\\"+rpl
 location2 = filepath+"\content\sounds\ouch.ogg"
 
 repo = "lolmanurfunny/Roblox-Launcher-minus-the-app"
-latest = urlopen("https://raw.githubusercontent.com/"+repo+"/main/Custom%20Launcher/latest").read().decode().rstrip(os.linesep)
+latest = urlopen("https://raw.githubusercontent.com/"+repo+"/main/Custom%20Launcher/latest").read().decode().rstrip(linesep)
 latestScript = urlopen("https://raw.githubusercontent.com/lolmanurfunny/Roblox-Launcher-minus-the-app/main/Custom%20Launcher/Nonproprietary%20Roblox%20Launcher.py")
 with open("RobloxPlayerLauncher.py", "wb") as f:
     f.write(latestScript.read())
 
-os.system('cmd /c "pyinstaller --onefile RobloxPlayerLauncher.py"')
-os.system('cmd /c "xcopy {}\\dist\\RobloxPlayerLauncher.exe  {} /y /s"'.format(absPath, filepath))
-os.system('cmd /c "del /f {}\\RobloxPlayerLauncher.py"'.format(absPath))
-os.system('cmd /c "rmdir /s /q {}\\build"'.format(absPath))
-os.system('cmd /c "del /f {}\\RobloxPlayerLauncher.spec"'.format(absPath))
-os.system('cmd /c "rmdir /s /q {}\\dist"'.format(absPath))
+system('cmd /c "pyinstaller --onefile RobloxPlayerLauncher.py"')
+system('cmd /c "xcopy {}\\dist\\RobloxPlayerLauncher.exe  {} /y /s"'.format(absPath, filepath))
+remove("{}\\RobloxPlayerLauncher.py".format(absPath))
+rmtree("{}\\build".format(absPath))
+remove("{}\\RobloxPlayerLauncher.spec".format(absPath))
+rmtree("{}\\dist".format(absPath))
 
 oof = input("Would you like to return the oof sound back? [Y/N] ")
 
-if oof.lower() == "y".strip():
+if oof.lower().strip() == "y":
     print("Installing oof sound...")
     urlretrieve("https://github.com/"+repo+"/raw/main/Audio/ouch.ogg",location2)
     print("Successfully installed oof sound.")
-elif oof.lower() == "n".strip():
+elif oof.lower().strip() == "n":
     print("Skipping...")
 else:
     print("You wrote neither \"Y\" nor \"N\". Skipping...")
@@ -66,7 +67,7 @@ else:
 print("This window will close in 3 seconds...")
 
 for _ in range(3,0,-1):
-    os.system("title "+"Closing in "+_.__str__())
+    system("title "+"Closing in "+_.__str__())
     print('.')
     sleep(1)
 exit(0) # now exiting with a non-non-zero value :D
