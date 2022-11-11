bc3e6058545c5d006fc8f1e072ba4677ea14cb20
localfossa <74490107+localfossa@users.noreply.github.com>
2022-11-11 23:20:15 +0300

    Update Patch Nabber (#69)
    * Update BetaApp Patch Nabber.py
    
    * Update

--- Patch Nabber/BetaApp Patch Nabber.py
+++ Patch Nabber/BetaApp Patch Nabber.py
@@ -1,14 +1,17 @@
 from sys import exit
 from time import sleep
-from os import getenv, system, rename
+from os import getenv, system
+from shutil import rmtree
 from os.path import exists
 from urllib.request import urlopen, urlretrieve
-from json import loads
+from json import load
 
-a = urlopen("https://clientsettingscdn.roblox.com/v2/client-version/WindowsPlayer/channel/zflag").read()
-a = loads(a)["clientVersionUpload"]
-print("Client version hash: "+a)
-filepath = getenv("LOCALAPPDATA")+"\Roblox\Versions\\"
+repo = "lolmanurfunny/Roblox-Launcher-minus-the-app"
+
+version = load(urlopen("https://clientsettingscdn.roblox.com/v2/client-version/WindowsPlayer/channel/zflag"))["clientVersionUpload"]
+
+print("Client version hash: "+version)
+filepath = getenv("LOCALAPPDATA")+"\\Roblox\\Versions\\"
 
 def littleTimmyPrevention():
     input("Did you install Roblox via running RobloxPlayerLauncher as administrator?\nTry reinstalling Roblox.\nDon't open a new issue regarding this if you installed Roblox to a non-User directory!")
@@ -16,9 +19,9 @@ def littleTimmyPrevention():
 debug = False
 if exists(filepath) and not debug:
     print("Found \"\Roblox\Versions\\\" folder!")
-    filepath+=a
+    filepath+=version
     if exists(filepath):
-        print("Found \""+a+"\" folder!")
+        print("Found \""+version+"\" folder!")
     else:
         print("[Error]: Unable to locate latest roblox client!")
         littleTimmyPrevention()
@@ -28,42 +31,23 @@ else:
     littleTimmyPrevention()
     exit(1)
 
-# rpl = "RobloxPlayerLauncher.exe"
+places = filepath+"\\ExtraContent\places"
+UniversalApp = filepath+"\\ExtraContent\\LuaPackages\\UniversalApp"
 
-# location1 = filepath+"\\"+rpl
-ExtraContent = filepath+"\ExtraContent"
+ouch = filepath+"\\content\\sounds\\ouch.ogg"
 
-places = ExtraContent+"\\places"
-# 🤓 (anyone else notice how there's no skin tone for the nerd emoji, bros perma asian)
-UniversalApp = ExtraContent+"\\LuaPackages\\UniversalApp"
-
-ouch = filepath+"\content\sounds\ouch.ogg"
-
-# places folder is the only one that matters here, but why not also prevent roblox from loading uNiVeRSaLaPp too while we're at it.
 if exists(places) and not debug:
-    rename(places,ExtraContent+"\\places_But_better") # not adding an "else" block if this doesn't work on your first execution per update (don't fucking make an issue)
-    if exists(UniversalApp):
-        rename(UniversalApp,ExtraContent+"\\LuaPackages\\"+"shitty idea on roblox's part ngl")
-# lord forgive me for doing this even though at the end of the day I'm still parting the sea.
-
-repo = "lolmanurfunny/Roblox-Launcher-minus-the-app"
-# latest = urlopen("https://raw.githubusercontent.com/"+repo+"/main/Custom%20Launcher/latest").read().decode().rstrip(linesep)
-#input(latest) # debuggerydoos
-# print("Installing latest custom launcher from github! Version: "+latest)
-# download = urlretrieve("https://github.com/"+repo+"/releases/download/"+latest+"/"+rpl,location1)
-
-# print("File is located @",download.__getitem__(0))
-
-
-
+    rmtree(places)
+if exists(UniversalApp) and not debug:
+    rmtree(UniversalApp)
 
 oof = input("Would you like to return the oof sound back? [Y/N] ")
 
-if oof.lower() == "y".strip():
+if oof.lower().strip() == "y":
     print("Installing oof sound...")
     urlretrieve("https://github.com/"+repo+"/raw/main/Audio/ouch.ogg",ouch)
     print("Successfully installed oof sound.")
-elif oof.lower() == "n".strip():
+elif oof.lower().strip() == "n":
     print("Skipping...")
 else:
     print("You wrote neither \"Y\" nor \"N\". Skipping...")
@@ -74,4 +58,4 @@ for _ in range(3,0,-1):
     system("title "+"Closing in "+_.__str__())
     print('.')
     sleep(1)
-exit(0) # now exiting with a non-non-zero value :D
+exit(0)
